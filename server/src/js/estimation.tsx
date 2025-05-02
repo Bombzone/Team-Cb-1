f6da73e830f7db63c6d2c951891279e407d51f2b
Bombzone <ryan.m.posey@gmail.com>
2025-05-02 02:25:30 -0600

    card-1

--- server/src/js/estimation.tsx
+++ server/src/js/estimation.tsx
@@ -90,7 +90,7 @@ const Table = () => {
         <>
             <h4 id="avg">{average ? average : "AVG"}</h4>
             <div id="players">
-                <Player name={players[0][0]} points={players[0][1]} id="player1" />
+                <Player name={players[0][0]} points={players[0][1] == "-1"? "":players[0][1]} id="player1" />
                 <Player name={players[1][0]} points={players[1][1]} id="player2" />
                 <Player name={players[2][0]} points={players[2][1]} id="player3" />
                 <Player name={players[3][0]} points={players[3][1]} id="player4" />
@@ -276,8 +276,16 @@ const EstimationButton = (props: { sendMessage: any, card: Card }) => { // retur
         // change to websocket send message
         sendMessage(`voted_${localStorage.getItem("UID")}_${props.card.getValue()}`);
     }
+    if (props.card.getValue() >= 0) {
+        return (
+            <li><button className="estButton" onClick={submit} value={props.card.getValue()}>{props.card.getValue()}</button></li>
+        )
+    }
     return (
-        <li><button className="estButton" onClick={submit} value={props.card.getValue()}>{props.card.getValue()}</button></li>
+        <li><button className="estButton" onClick={submit} value={props.card.getValue()}><svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="black" viewBox="0 0 16 16">
+        <path d="M14.778.085A.5.5 0 0 1 15 .5V8a.5.5 0 0 1-.314.464L14.5 8l.186.464-.003.001-.006.003-.023.009a12 12 0 0 1-.397.15c-.264.095-.631.223-1.047.35-.816.252-1.879.523-2.71.523-.847 0-1.548-.28-2.158-.525l-.028-.01C7.68 8.71 7.14 8.5 6.5 8.5c-.7 0-1.638.23-2.437.477A20 20 0 0 0 3 9.342V15.5a.5.5 0 0 1-1 0V.5a.5.5 0 0 1 1 0v.282c.226-.079.496-.17.79-.26C4.606.272 5.67 0 6.5 0c.84 0 1.524.277 2.121.519l.043.018C9.286.788 9.828 1 10.5 1c.7 0 1.638-.23 2.437-.477a20 20 0 0 0 1.349-.476l.019-.007.004-.002h.001M14 1.221c-.22.078-.48.167-.766.255-.81.252-1.872.523-2.734.523-.886 0-1.592-.286-2.203-.534l-.008-.003C7.662 1.21 7.139 1 6.5 1c-.669 0-1.606.229-2.415.478A21 21 0 0 0 3 1.845v6.433c.22-.078.48-.167.766-.255C4.576 7.77 5.638 7.5 6.5 7.5c.847 0 1.548.28 2.158.525l.028.01C9.32 8.29 9.86 8.5 10.5 8.5c.668 0 1.606-.229 2.415-.478A21 21 0 0 0 14 7.655V1.222z"/>
+      </svg></button></li>
+
     )
 }
 export default Estimation;
